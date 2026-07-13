4198ee8e2d3eac60dcf303b03226f63c41e9c672
psu6810110229 <6810110229@psu.ac.th>
2026-07-13 21:12:19 +0700

    style: reduce dock max scale from 1.3 to 1.15 for a subtler effect

--- src/components/DockItem/DockItem.tsx
+++ src/components/DockItem/DockItem.tsx
@@ -27,7 +27,7 @@ export function DockItem({ mouseX, mouseY, isDesktop, as = 'div', children, clas
     return Math.sqrt(dx * dx + dy * dy);
   });
 
-  const scaleSync = useTransform(distance, [0, 140], [1.3, 1], { clamp: true });
+  const scaleSync = useTransform(distance, [0, 140], [1.15, 1], { clamp: true });
   const scale = useSpring(scaleSync, { mass: 0.1, stiffness: 400, damping: 20 });
 
   const props = {
